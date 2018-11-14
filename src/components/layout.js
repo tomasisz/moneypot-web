aec9f7d134028c6068861191efb02c45012dcfe2
Jenny Taylor <sugarfreecow@gmail.com>
2018-11-14 11:18:12 -0800

    Changed title

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ export default ({ children }) => (
     <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
         <header style={{ marginBottom: `1.5rem` }}>
             <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-                <h3 style={{ display: `inline` }}>MySweetSite</h3>
+                <h3 style={{ display: `inline` }}>Hookedin.com</h3>
             </Link>
             <ul style={{ listStyle: `none`, float: `right` }}>
                 <ListLink to="/">Home</ListLink>
