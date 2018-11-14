ec89773116f9c3e81cd25c462a5f7f2a552abde9
Jenny Taylor <sugarfreecow@gmail.com>
2018-11-14 12:53:01 -0800

    Adding a new set up

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,60 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+import icon16 from '../../static/favicons/favicon-16x16.png'
+import icon32 from '../../static/favicons/favicon-32x32.png'
+import favicon from '../../static/favicons/favicon.ico'
 
-const ListLink = props => (
-    <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-        <Link to={props.to}>{props.children}</Link>
-    </li>
+
+import Header from './header'
+import './layout.css'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+          link={[
+              { rel: 'icon', type: 'image/png', sizes: "16x16", href: `${icon16}` },
+              { rel: 'icon', type: 'image/png', sizes: "32x32", href: `${icon32}` },
+              { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
 )
-export default ({ children }) => (
-    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
-        <header style={{ marginBottom: `1.5rem` }}>
-            <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-                <h3 style={{ display: `inline` }}>Hookedin.com</h3>
-            </Link>
-            <ul style={{ listStyle: `none`, float: `right` }}>
-                <ListLink to="/">Home</ListLink>
-                <ListLink to="/about/">About</ListLink>
-                <ListLink to="/contact/">Contact</ListLink>
-            </ul>
-        </header>
-        {children}
-    </div>
-)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
