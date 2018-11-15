9fef8a3f37daec5143f051b6274c04a1503d62a3
Ekene Madu <EkeneMadu@protonmail.com>
2018-11-14 22:19:25 -0800

    Integrate hookedin-lib

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,47 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import hi from 'hookedin-lib'
 
 import Layout from '../components/layout'
 
+
+
+
+class DemoPrivKey extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.newPrivkey = this.newPrivkey.bind(this);
+
+    this.state = {
+      privkey: hi.crypto.PrivateKey.fromRand()
+    };
+  }
+
+  newPrivkey() {
+    this.setState({ privkey: hi.crypto.PrivateKey.fromRand() });
+  }
+
+  render() {
+    console.log('render recalled')
+    return (
+      <div>
+        <h3>{ this.state.privkey.toBech() }</h3>
+        <button onClick={ this.newPrivkey }>New!</button>
+      </div>
+    );
+  }
+}
+
+
+
+
+
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>This is what a hookedin private key looks:</h1>
+    <DemoPrivKey/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
