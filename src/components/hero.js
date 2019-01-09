560b79d565078e669ed3894c96535d0cd3e5b626
Jenny Taylor <sugarfreecow@gmail.com>
2019-01-09 12:39:22 -0800

    Some small layout changes

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,11 +5,8 @@ const Hero = () => (
         style={{
             background: 'linear-gradient(rgb(15, 98, 189) 24%, rgb(10, 64, 122) 168%)',
             backgroundImage: 'linear-gradient(rgb(15, 98, 189) 24%, rgb(10, 64, 122) 168%)',
-            borderBottomLeftRadius: '50% 8%',
-            borderBottomRightRadius: '50% 8%',
             minHeight: '90vh',
-            width: '120%',
-            margin: '-10% -10% 0% -10%',
+            margin: '-5rem -1rem 0',
         }}
     >
         <div
