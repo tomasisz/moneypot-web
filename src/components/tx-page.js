f4507599168f0546e59dbb3490d90027462790ff
Jenny Taylor <sugarfreecow@gmail.com>
2019-02-09 14:46:01 -0800

    Added info from API

---
+++ src/components/tx-page.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import SectionDiv from '../components/section-div'
+import './tx-page.css'
+
+
+class TxPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            height: 0,
+            blockHash: '',
+            size: 0,
+            weight: 0,
+            fee: 0,
+            version: 0,
+            locktime: 0,
+        };
+    }
+
+    componentDidMount(props) {
+        fetch("https://blockstream.info/testnet/api/tx/11440bb2493d3f3ce5c4932bd79dd89c408b9dd7b5affdb0ec7b5434e0eb8ae8")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log('the api info is: ',result);
+                    this.setState({
+                        isLoaded: true,
+                        height: result.status.block_height,
+                        blockHash: result.status.block_hash,
+                        size: result.size,
+                        weight: result.weight,
+                        fee: result.fee,
+                        version: result.version,
+                        locktime: result.locktime,
+
+
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+
+    }
+
+    render(props) {
+        const { error, isLoaded, height, blockHash, size, weight, fee, version, locktime } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+
+                    <SectionDiv>
+                        <h2>Transaction {this.props.page}</h2>
+
+                        <div className="tx-stats-table">
+                            <div>
+                                <div>Block Height</div>
+                                <div>{ height }</div>
+                            </div>
+                            <div>
+                                <div>Included in Block</div>
+                                <div><a href="">{ blockHash }</a></div>
+                            </div>
+                            <div>
+                                <div>Size (bytes)</div>
+                                <div>{ size }</div>
+                            </div>
+                            <div>
+                                <div>Virtual size (vbytes)</div>
+                                <div>???</div>
+                            </div>
+                            <div>
+                                <div>Weight units (WU)</div>
+                                <div>{ weight }</div>
+                            </div>
+                            <div>
+                                <div>Transaction fees</div>
+                                <div>{parseFloat(fee / 100000000).toFixed(8)} tBTC (101 sat/vB)</div>
+                            </div>
+                            <div>
+                                <div>Version</div>
+                                <div>{ version }</div>
+                            </div>
+                            <div>
+                                <div>Lock time</div>
+                                <div>{ locktime }</div>
+                            </div>
+                        </div>
+
+                    </SectionDiv>
+                </div>
+            );
+        }
+    }
+}
+
+
+
+export default TxPage
+
--- src/pages/explore.js
+++ src/pages/explore.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import SectionDiv from '../components/section-div'
 import { Router, navigate } from "@reach/router"
-
+import TxPage from '../components/tx-page'
 
 
 const App = () => (
@@ -12,7 +12,7 @@ const App = () => (
                 <QueryTx path="explore/" />
                 <QueryTx path="explore/tbtc/" />
                 <QueryTx path="explore/tbtc/tx/" />
-                <Tx path="explore/tbtc/tx/:page" />
+                <TxPage path="explore/tbtc/tx/:page" />
             </Router>
         </SectionDiv>
     </Layout>
@@ -56,12 +56,6 @@ class QueryTx extends React.Component {
     }
 }
 
-
-const Tx = props => (
-    <div>
-        <h1>Explore: {props.page}</h1>
-    </div>
-);
 
 export default App
 
