925e33c9f5a16e1ba2ef38843efa40399dbabc26
Jenny Taylor <sugarfreecow@gmail.com>
2019-02-25 12:39:27 -0800

    Work in progress in tx page

--- src/components/tx-page.js
+++ src/components/tx-page.js
@@ -34,10 +34,8 @@ class TxPage extends React.Component {
         this.setState({collapse: !this.state.collapse});
     }
 
-
-    componentDidMount(props) {
-        console.log('a transaction is: 11440bb2493d3f3ce5c4932bd79dd89c408b9dd7b5affdb0ec7b5434e0eb8ae8');
-        fetch("https://blockstream.info/testnet/api/tx/"+this.props.page)
+    getTransactionInfo(txid){
+        fetch("https://blockstream.info/testnet/api/tx/"+txid)
             .then(res => res.json())
             .then(
                 (result) => {
@@ -68,8 +66,20 @@ class TxPage extends React.Component {
 
                 }
             )
-
     }
+
+    componentDidMount() {
+        console.log('a transaction is: 11440bb2493d3f3ce5c4932bd79dd89c408b9dd7b5affdb0ec7b5434e0eb8ae8');
+        console.log('a coinbase txid is: 35e78b61dce421c93cc476a90e2f416ffd13f6e70527b178617c888ebc43f0ff');
+        this.getTransactionInfo(this.props.page)
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.page !== prevProps.page) {
+            this.getTransactionInfo(this.props.page);
+        }
+    }
+
 
     render(props) {
         const { collapse, error, isLoaded, height, blockHash, size, weight, fee, version, locktime, vin, vout } = this.state;
@@ -78,12 +88,15 @@ class TxPage extends React.Component {
         } else if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
+            const vBytes = Math.ceil(weight / 4);
+            const feeRate = fee/ vBytes;
+
             return (
                 <div>
                     <SectionDiv>
                         <h2>Transaction {this.props.page}</h2>
                         <div className="tx-stats-table">
-                            <div>
+                            <div style={{ color: 'red' }}>
                                 <div>Block Height</div>
                                 <div>{ height }</div>
                             </div>
@@ -96,8 +109,8 @@ class TxPage extends React.Component {
                                 <div>{ size }</div>
                             </div>
                             <div>
-                                <div>Virtual size (vbytes)</div>
-                                <div>{ Math.ceil(weight / 4 )}</div>
+                                <div>Virtual size (vB)</div>
+                                <div>{ vBytes }</div>
                             </div>
                             <div>
                                 <div>Weight units (WU)</div>
@@ -105,7 +118,7 @@ class TxPage extends React.Component {
                             </div>
                             <div>
                                 <div>Transaction fees</div>
-                                <div>{parseFloat(fee / 100000000).toFixed(8)} tBTC (101 sat/vB)</div>
+                                <div>{parseFloat(fee / 100000000).toFixed(8)} tBTC ({ feeRate.toFixed(0) } sat/vB)</div>
                             </div>
                             <div>
                                 <div>Version</div>
@@ -119,48 +132,44 @@ class TxPage extends React.Component {
                     </SectionDiv>
                     <SectionDiv>
                         <Row>
-                            <Col>
                                 <h2>Inputs & Outputs</h2>
-                            </Col>
-                            <Col style={{ textAlign: 'right'}}>
-                                <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Details { collapse ?  '-' : '+' }</Button>
-                            </Col>
+
                         </Row>
                         <Row>
                             <Col>
+                                <Row>
+                                    <p style={{ textAlign: 'center' }}> { vin.length } Input{ vin.length > 1 ? 's' : ''} Consumed</p>
+                                </Row>
                                 <div className="tx-input-and-output-table">
                                                 {
-                                                    vin.map(function(item, i){
-                                                        return (
-                                                            <div key={i}>
-                                                                <div>#{i}</div>
-                                                                <div><a href={"/explore/tbtc/tx/"+item.txid}>{item.txid}:{item.vout}</a></div>
-                                                                <div>{ item.prevout.value/100000000 } tBTC</div>
-                                                            </div>
-                                                        );
-                                                    })
+                                                    vin.map(displayInput)
+                                                    // This would be the same:
+                                                   //vin.map((item, i)=> displayInput(item, i))
                                                 }
 
-                                        <Collapse isOpen={this.state.collapse} style={{ color: 'red' }}>
-                                            TO DO
-                                        </Collapse>
                                 </div>
                             </Col>
                             <Col xs={1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                                 <FontAwesomeIcon icon="chevron-right" />
                             </Col>
                             <Col>
+                                <Row>
+                                    <Col>
+                                        <p style={{ textAlign: 'center' }}> { vout.length } Output{ vout.length > 1 ? 's' : ''} Created</p>
+                                    </Col>
+                                    <Col>
+                                        <Col style={{ textAlign: 'right'}}>
+                                            <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Details { collapse ?  '-' : '+' }</Button>
+                                        </Col>
+                                    </Col>
+                                </Row>
                                 <div className="tx-input-and-output-table">
+                                    <Collapse isOpen={this.state.collapse} style={{ color: 'red' }}>
+                                        TO DO
+                                    </Collapse>
+
                                     {
-                                        vout.map(function(item, i){
-                                            return (
-                                                <div key={i}>
-                                                    <div>#{i}</div>
-                                                    <div>{item.scriptpubkey_address}</div>
-                                                    <div>{ item.value/100000000 } tBTC</div>
-                                                </div>
-                                            );
-                                        })
+                                        vout.map(displayOutput)
                                     }
                                 </div>
                             </Col>
@@ -172,7 +181,39 @@ class TxPage extends React.Component {
     }
 }
 
+function displayInput(item,i) {
+    if ( item.is_coinbase ) {
+        return <div key={i}>coinbase</div>
+    }
+    return (
+        <div key={i} id={ 'spent-by-'+item.txid}>
+            <div>#{i}</div>
+            <div>
+                {item.prevout.value / 100000000} tBTC from {' '}
+                <Link to={"/explore/tbtc/address/" + item.prevout.scriptpubkey_address}>{item.prevout.scriptpubkey_address}</Link>{' '}
+                (<Link to={"/explore/tbtc/tx/" + item.txid +"/#output-index-"+item.vout}>prev-output</Link>)
+            </div>
+        </div>
+    );
+}
+
+function displayOutput(item, i){
+    return (
+        <div key={i} id={ 'output-index-'+i}>
+            <div>#{i}</div>
+            <div>
+                { item.value/100000000 } tBTC to {' '}
+                <Link to={"/explore/tbtc/address/" + item.scriptpubkey_address}>{item.scriptpubkey_address}</Link>{' '}
+                (<Link to={"/explore/tbtc/tx/" + item.txid +"/#spent-by-"+item.vout}>spent</Link>)
+
+            </div>
+        </div>
+    );
+}
 
 
 export default TxPage
 
+// TO DO:
+// In the links to other transactions we should be using Link and make sure all the API info updates
+// Collapse / detail
